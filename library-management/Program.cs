eb9bf0c723a8ebbae271eb4fe4c5b1d6c3523b70
sukant43 <sukant98657@gmail.com>
2025-07-16 13:01:17 +0530

    add changes

--- library-management/Program.cs
+++ library-management/Program.cs
@@ -1,1 +1,37 @@
-﻿
+﻿public class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter 1 to access the admin section or 2 to access the user section. Enter 3 to exit.");
+        Console.Write("Your choice: ");
+        int choice = int.Parse(Console.ReadLine());
+        while (choice != -1)
+        {
+            if (choice == 1)
+            {
+                Console.WriteLine("You have chosen to access the admin section for access you need to enter the UserName and Password.");
+
+                choice = int.Parse(Console.ReadLine());
+                if (choice == 1)
+                {
+                    
+                }
+            }
+            else if (choice == 2)
+            {
+
+            }
+            else if (choice == 3)
+            {
+                Console.WriteLine("Exiting the program. Goodbye!");
+                break;
+            }
+
+            else
+            {
+                Console.WriteLine("Invalid choice. Please enter 1 or 2. Try again.");
+            }
+        }
+
+    }
+} 
--- library-management/obj/Debug/net9.0/..AssemblyInfo.cs
+++ library-management/obj/Debug/net9.0/..AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute(".")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4373703b526cca16e97f989291bf17f36a615182")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+69f07a59936311f1b2b116977bd7cbdbc7f70b84")]
 [assembly: System.Reflection.AssemblyProductAttribute(".")]
 [assembly: System.Reflection.AssemblyTitleAttribute(".")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
