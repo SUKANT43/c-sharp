1a2a37ab22204a73171fb38ffea9ab889b31e8fa
sukant43 <sukant98657@gmail.com>
2025-07-21 13:57:34 +0530

    increment decrement finished

---
+++ WpfApp1/WpfApp1/IncDec.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfApp1
+{
+    public partial class IncDec : UserControl
+    {
+        int count = 0;
+
+        public IncDec()
+        {
+            InitializeComponent();
+        }
+
+        private void Increment(object sender, RoutedEventArgs e)
+        {
+            count++;
+            CounterText.Text = count.ToString();
+        }
+
+        private void Decrement(object sender, RoutedEventArgs e)
+        {
+            if (count < 0)
+            {
+                CounterText.Text = "Number must be greater than or equal to zero";
+                return;
+            }
+            count--;
+            CounterText.Text = count.ToString();
+        }
+    }
+}
