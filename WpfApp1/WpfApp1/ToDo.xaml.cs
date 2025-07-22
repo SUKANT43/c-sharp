83895bdb50c8fa4c44b0ae2d027adc07f2718085
sukant43 <sukant98657@gmail.com>
2025-07-22 15:45:45 +0530

    todo ui completed

---
+++ WpfApp1/WpfApp1/ToDo.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for ToDo.xaml
+    /// </summary>
+    public partial class ToDo : UserControl
+    {
+        List<BluePrint> list = new List<BluePrint>();
+        public ToDo()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (InputValue.Text == "")
+            {
+                return;
+            }
+            else
+            {
+                list.Add(new BluePrint(InputValue.Text,false));
+                InputValue.Text = "";
+            }
+        }
+    }
+
+    public class BluePrint
+    {
+        string value;
+        bool done;
+
+        public BluePrint(string value, bool done)
+        {
+            this.value = value;
+            this.done = done;
+        }
+    }
+}
