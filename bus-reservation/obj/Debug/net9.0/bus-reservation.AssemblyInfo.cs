173ab4487ccdcb34407211d17801edff9370be69
sukant43 <sukant98657@gmail.com>
2025-07-25 11:06:44 +0530

    library management starter added

--- bus-reservation/obj/Debug/net9.0/bus-reservation.AssemblyInfo.cs
+++ bus-reservation/obj/Debug/net9.0/bus-reservation.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("bus-reservation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0802c40f5590125ad6fb651e6c87f70a64dc1cac")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0a311face1b769311114966a23f5a888074939d")]
 [assembly: System.Reflection.AssemblyProductAttribute("bus-reservation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("bus-reservation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
