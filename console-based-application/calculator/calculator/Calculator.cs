d866c18d46c79512529280b5de29a86cfb120c37
sukant43 <sukant98657@gmail.com>
2025-07-28 11:21:57 +0530

    exe calculator finished

---
+++ console-based-application/calculator/calculator/Calculator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculator
+{
+    class Calculator
+    {
+        public static int Add(int a,int b)
+        {
+            return a + b;
+        }
+        public static int Subract(int a,int b)
+        {
+            return a - b;
+        }
+        public static int Multiply(int a,int b)
+        {
+            return a * b;
+        }
+        public static int Division(int a,int b)
+        {
+            return a / b;
+        }
+    }
+}
--- console-based-application/calculator/calculator/Program.cs
+++ console-based-application/calculator/calculator/Program.cs
@@ -8,8 +8,51 @@ namespace calculator
 {
     class Program
     {
+
+        public static void Solution(int result)
+        {
+            Console.WriteLine("Your Answer: "+ result);
+        }
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter 1 to Add, 2 to Subract , 3 to Multiply, 4 to Division");
+            Console.WriteLine("Enter Your Choice");
+            int choice = int.Parse(Console.ReadLine());
+            if (choice > 4 || choice < 1)
+            {
+                Console.WriteLine("Choose the given choice.");
+                Console.ReadLine();
+                return;
+            }
+            Console.Write("Enter Number a:");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("Enter Number b:");
+            int b = int.Parse(Console.ReadLine());
+
+            if (choice == 1)
+            {
+                Solution(Calculator.Add(a, b));
+            }
+            else if (choice == 2)
+            {
+                Solution(Calculator.Subract(a, b));
+            }
+            else if (choice == 3)
+            {
+                Solution(Calculator.Multiply(a, b));
+            }
+            else if (choice == 4)
+            {
+                if (b == 0)
+                {
+                    Console.WriteLine("Cannot divide by zero.");
+                    Console.ReadLine();
+                    return;
+                }
+                    Solution(Calculator.Division(a, b));
+            }
+            Console.ReadLine();
         }
+
     }
 }
