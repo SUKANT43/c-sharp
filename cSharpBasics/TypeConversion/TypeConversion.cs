87e9a005d22e763a7c0ef0461a838e002d5435ac
sukant43 <sukant98657@gmail.com>
2025-07-29 15:45:02 +0530

    type conversion

--- cSharpBasics/TypeConversion/TypeConversion.cs
+++ cSharpBasics/TypeConversion/TypeConversion.cs
@@ -10,23 +10,32 @@ namespace cSharpBasics
     {
         public static void print()
         {
+            Console.WriteLine("String");
             int num = 10;
             string a = Convert.ToString(num);
             Console.WriteLine(a);
             string b = num.ToString();
             Console.WriteLine(b);
 
+            Console.WriteLine("Integer");
             string str = "10";
             int c = (int)Convert.ToInt32(str);
             Console.WriteLine(c);
             int e = int.Parse(str);
             Console.WriteLine(e);
+            int g;
+            bool h = int.TryParse(str,out g);
+            Console.WriteLine(g);
 
+            Console.WriteLine("Decimal");
             string str2 = "20.4";
             double d = Convert.ToDouble(str2);
             Console.WriteLine(d);
             double f = double.Parse(str2);
             Console.WriteLine(f);
+            double i;
+            bool j = double.TryParse(str2, out i);
+            Console.WriteLine(i);
 
 
             Console.ReadLine();
