bf4cb3132f3ce4f12ec8827b7d2eb33a5329f7f1
SUKANT43 <sukant98657@gmail.com>
2025-08-18 11:53:01 +0530

    linq

---
+++ cSharpBasics/Linq/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8 };
+
+            var evens = from num in numbers
+                        where num % 2 == 0
+                        select num;
+            Console.WriteLine(string.Join(", ", evens));
+            Console.ReadLine();
+        }
+    }
+}
