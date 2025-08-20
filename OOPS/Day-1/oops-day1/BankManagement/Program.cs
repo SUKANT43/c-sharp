56885f6839b50bdda13183a1773729665d0ac112
SUKANT43 <sukant98657@gmail.com>
2025-08-20 12:52:10 +0530

    bank management

--- OOPS/Day-1/oops-day1/BankManagement/Program.cs
+++ OOPS/Day-1/oops-day1/BankManagement/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BankManagement
 {
@@ -10,6 +7,33 @@ namespace BankManagement
     {
         static void Main(string[] args)
         {
+            foreach (var user in setUserDetails.userDetailList)
+            {
+                Console.WriteLine($"Account: {user.AccNo}, Name: {user.Pass}, Balance: {user.checkBalance}");
+            }
+
+            Console.WriteLine("\n--- Performing Transactions ---");
+
+            var sukant = setUserDetails.userDetailList[0];
+
+            sukant.Credit = 5000;
+            Console.WriteLine($"After crediting 5000, Balance of {sukant.Pass}: {sukant.checkBalance}");
+
+            sukant.Debit = 3000;
+            Console.WriteLine($"After debiting 3000, Balance of {sukant.Pass}: {sukant.checkBalance}");
+
+            sukant.Debit = 200000; 
+            Console.WriteLine($"After trying overdraft, Balance of {sukant.Pass}: {sukant.checkBalance}");
+
+            sukant.Pass = "Sukant C";
+            Console.WriteLine($"Updated Name: {sukant.Pass}");
+
+            Console.WriteLine("\n--- Final Account Details ---");
+            foreach (var user in setUserDetails.userDetailList)
+            {
+                Console.WriteLine($"Account: {user.AccNo}, Name: {user.Pass}, Balance: {user.checkBalance}");
+            }
+            Console.ReadLine();
         }
     }
 }
---
+++ OOPS/Day-1/oops-day1/BankManagement/UserDetails.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankManagement
+{
+    class UserDetails
+    {
+        private string accNo;
+        private string name;
+        private long balance;
+
+        public UserDetails(string accNo, string name, long balance)
+        {
+            this.accNo = accNo;
+            this.name = name;
+            this.balance = balance;
+        }
+
+        public string AccNo
+        {
+            get { return accNo; }
+            set { accNo = value; }
+        }
+
+        public string Pass
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public long Credit
+        {
+            set {if(value>0) balance+= value;}
+        }
+
+        public long Debit
+        {
+            set {if(balance>=value) balance -= value; }
+        }
+
+        public long checkBalance
+        {
+            get { return balance; }
+        }
+    }
+
+    class setUserDetails
+    {
+        public static List<UserDetails> userDetailList = new List<UserDetails>
+        {
+            new UserDetails("ACC1001", "Sukant",100023),
+            new UserDetails("ACC1002", "Vijay",150023),
+            new UserDetails("ACC1003", "Ranjith",23000)
+        };
+
+    }
+}
