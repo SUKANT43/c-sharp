305b8d1c626f635a7efc72209317c74cd574272c
SUKANT43 <sukant98657@gmail.com>
2025-08-21 10:24:36 +0530

    interface

---
+++ OOPS/Day-3/Day3/Day3/Interface.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day3
+{
+   interface Vehicles
+    {
+        void Start();
+        void Stop();
+        int GetSpeed();
+    }
+
+    class Car : Vehicles
+    {
+        private int Speed;
+        public void Start()
+        {
+            Speed = 40;
+            Console.WriteLine("Car Stopped = " + Speed + " Km/h");
+        }
+
+        public void Stop()
+        {
+            Speed = 0;
+            Console.WriteLine("Car stopped.");
+        }
+
+        public int GetSpeed()
+        {
+            return Speed;
+        }
+    }
+
+    class Bike : Vehicles
+    {
+        private int speed;
+
+        public void Start()
+        {
+            speed = 20;
+            Console.WriteLine("Bike started. Speed = " + speed + " km/h");
+        }
+
+        public void Stop()
+        {
+            speed = 0;
+            Console.WriteLine("Bike stopped.");
+        }
+
+        public int GetSpeed()
+        {
+            return speed;
+        }
+    }
+
+
+    class Disp1
+    {
+        public static void disp1()
+        {
+            Vehicles car = new Car();
+            car.Start();
+            Console.WriteLine("Car Speed: " + car.GetSpeed());
+            car.Stop();
+
+            Console.WriteLine();
+
+            Vehicles bike = new Bike();
+            bike.Start();
+            Console.WriteLine("Bike Speed: " + bike.GetSpeed());
+            bike.Stop();
+
+        }
+    }
+}
