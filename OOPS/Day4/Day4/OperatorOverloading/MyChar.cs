ea075b4c936f5d371f8c0367f5bbfd5b2170aa77
SUKANT43 <sukant98657@gmail.com>
2025-08-22 10:42:59 +0530

    operator overloading

---
+++ OOPS/Day4/Day4/OperatorOverloading/MyChar.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorOverloading
+{
+    class MyChar
+    {
+        private char ch;
+        public MyChar(char ch)
+        {
+            this.ch = ch;
+        }
+
+        public static bool operator ==(MyChar c1,MyChar c2)
+        {
+            return c1.ch == c2.ch;
+        }
+
+        public static bool operator !=(MyChar c1,MyChar c2)
+        {
+            return c1.ch != c2.ch;
+        }
+
+
+    }
+
+    class Disp
+    {
+        public static void disp()
+        {
+            MyChar c1 = new MyChar('a');
+            MyChar c2 = new MyChar('a');
+            Console.WriteLine(c1 == c2);
+            Console.WriteLine(c1 != c2);
+        }
+    }
+}
---
+++ OOPS/Day4/Day4/OperatorOverloading/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorOverloading
+{
+    class Ticket
+    {
+        public int DurationInHours{ set; get; }
+        public DateTime CreationDone { get; set; }
+        public Ticket(int DurationInHours)
+        {
+            this.DurationInHours = DurationInHours;
+            this.CreationDone = DateTime.Now;
+        }
+
+        public static Ticket operator+(Ticket a,Ticket b)
+        {
+            return new Ticket(a.DurationInHours + b.DurationInHours);
+        }
+    }
+
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Ticket firstTicket = new Ticket(3);
+            Ticket secondtTicket = new Ticket(4);
+
+            Ticket sumTicket = firstTicket + secondtTicket;
+            Console.WriteLine(sumTicket.DurationInHours);
+            Disp.disp();
+            Console.ReadLine();
+        }
+    }
+}
