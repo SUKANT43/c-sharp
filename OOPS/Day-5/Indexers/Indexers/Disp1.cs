0fdbc7166a8fd0ebfe3b6f0e6f34c02cf3ca70e3
SUKANT43 <sukant98657@gmail.com>
2025-08-25 12:05:38 +0530

    indexers

---
+++ OOPS/Day-5/Indexers/Indexers/Disp1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Indexers
+{
+    class MySolution
+    {
+        int[] arr = new int[5];
+
+        public int this[int num]
+        {
+            set
+            {
+                arr[num] = value;
+            }
+            get
+            {
+                return arr[num];
+            }
+        }
+    }
+    class Disp1
+    {
+        public static void disp1()
+        {
+            MySolution sol = new MySolution();
+            sol[0] = 10;
+            sol[1] = 100;
+            Console.WriteLine(sol[0]);
+        }
+    }
+}
---
+++ OOPS/Day-5/Indexers/Indexers/Disp2.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Indexers
+{
+    class EmployeeDictionary
+    {
+     private Dictionary<string ,string>emp=new Dictionary<string,string>();
+        public string this[string id]
+        {
+            set
+            {
+                emp[id] = value;
+            }
+            get
+            {
+                if (emp.ContainsKey(id))
+                {
+                    return emp[id];
+                }
+                return "Emp not found";
+            }
+        }
+    }
+
+    class Disp2
+    {
+        public static void disp2()
+        {
+            EmployeeDictionary emp = new EmployeeDictionary();
+            emp["ec340"] = "vijay";
+            emp["ec326"] = "sukant";
+            Console.WriteLine(emp["ec326"]);
+            Console.WriteLine(emp["ec341"]);
+
+        }
+    }
+}
