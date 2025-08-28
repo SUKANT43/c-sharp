f4c146dc91dcd08797c88ae51527320d47c4cbb5
SUKANT43 <sukant98657@gmail.com>
2025-08-28 12:05:09 +0530

    models created

---
+++ BankManagement/BankManagement/Model/Customer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankManagement.Model
+{
+    class Customer
+    {
+       public string Account_number { get; set; }
+       public string UserName { get; set; }
+       public string Password { get; set; }
+    }
+}
---
+++ BankManagement/BankManagement/Model/TransactionHistory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankManagement.Model
+{
+    class TransactionHistory
+    {
+       public string Account_Number { get; set; }
+       public int ToAccount { get; set; }
+       public int TransactionAmount { get; set; }
+    }
+}
