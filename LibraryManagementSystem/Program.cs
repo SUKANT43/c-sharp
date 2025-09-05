d86664efffbcc95442a96c708ae6c08672b6eb2e
sukant43 <sukant98657@gmail.com>
2025-09-05 15:47:23 +0530

    printed book class finished

--- LibraryManagementSystem/Program.cs
+++ LibraryManagementSystem/Program.cs
@@ -34,6 +34,28 @@ abstract class Book
         get => year;
         set => year = value;
     }
-    
 
+    protected Book(string title, string author, int year)
+    {
+        Title = title;
+        Author = author;
+        Year = year;
+    }
+
+    public void Borrow() => IsBorrowed = true;
+    public void Return() => IsBorrowed = false;
+
+    public abstract void Display();
+
+}
+class PrintedBook : Book
+{
+    public PrintedBook(string title, string author, int year) : base(title, author, year) { }
+
+    public override void Display()
+    {
+        Console.WriteLine($"[Printed Book] {Title} by {Author} ({Year})");
+    }
+    
+    
 }
