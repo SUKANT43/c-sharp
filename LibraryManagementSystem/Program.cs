fe81c26dedbc35e1fa5a4eba08c9b1328adc6c3a
sukant43 <sukant98657@gmail.com>
2025-09-05 15:36:34 +0530

    book fields finished

--- LibraryManagementSystem/Program.cs
+++ LibraryManagementSystem/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+
+        Console.ReadLine();
+    }
+}
+
+abstract class Book
+{
+    private string title;
+    private string author;
+    private int year;
+    public bool IsBorrowed { get; private set; }
+
+    public string Title
+    {
+        get
+        {
+            return title;
+        }
+        set => title = value;
+    }
+
+    public string Author
+    {
+        get => author;
+        set => author = value;
+    }
+
+    public int Year
+    {
+        get => year;
+        set => year = value;
+    }
+    
+
+}
