534f40e89726bd5a11bd814b4f3e532897cc1375
SUKANT43 <sukant98657@gmail.com>
2025-09-09 12:52:03 +0530

    events and delegates are added

--- User_Management/User_Management/Program.cs
+++ User_Management/User_Management/Program.cs
@@ -8,9 +8,43 @@ namespace User_Management
         {
             //Service.AddUser("sukant","sukant.ec22@bitsathy.ac.in",18);
             //Service.DeleteUser(1);
-            Service.UpdateUser(2,"sukant","sukant98657@gmail.com",22);
+            //Service.UpdateUser(2,"sukant","sukant98657@gmail.com",22);
             Service.GetUserData();
+
+            Create.ev += Print;
+            Create.create("sukant");
+            Create.create("vijay");
+
+
             Console.ReadLine();
         }
+
+        public static void Print(object e,Demo d)
+        {
+            Console.WriteLine(d.name);
+        }
     }
+
+    class Demo : EventArgs
+    {
+        public string name;
+        public Demo(string na)
+        {
+            name = na;
+        }
+    }
+
+    class Create
+    {
+        public static event EventHandler<Demo> ev;
+
+        public static void create(string n)
+        {
+            Demo d = new Demo(n);
+            ev?.Invoke(null, d);
+        }
+    }
+
+    
+
 }
