2f8da83232f4b70fe1af98a2e64d60047e924201
SUKANT43 <sukant98657@gmail.com>
2026-04-06 10:55:11 +0530

    events and delegates revised

--- EventsAndDelegates/EventsAndDelegates/Class3.cs
+++ EventsAndDelegates/EventsAndDelegates/Class3.cs
@@ -6,52 +6,56 @@ using System.Threading.Tasks;
 
 namespace EventsAndDelegates
 {
-    public class FileEventArgs : EventArgs
-    {
-        public string FileName { get; }
-        public FileEventArgs(string fileName)
-        {
-            FileName = fileName;
-        }
-    }
+    //public class FileEventArgs : EventArgs
+    //{
+    //    public string FileName { get; }
+    //    public FileEventArgs(string fileName)
+    //    {
+    //        FileName = fileName;
+    //    }
+    //}
 
-    // Publisher
-    public class FileUploader
-    {
-        // Step 2: Declare event with EventHandler<T>
-        public event EventHandler<FileEventArgs> FileUploaded;
+    //// Publisher
+    //public class FileUploader
+    //{
+    //    // Step 2: Declare event with EventHandler<T>
+    //    public event EventHandler<FileEventArgs> FileUploaded;
 
-        // Step 3: Raise event
-        public void Upload(string fileName)
-        {
-            Console.WriteLine($"Uploading {fileName}...");
+    //    // Step 3: Raise event
+    //    public void Upload(string fileName)
+    //    {
+    //        Console.WriteLine($"Uploading {fileName}...");
 
-            // Step 4: Invoke event safely
-            FileUploaded?.Invoke(this, new FileEventArgs(fileName));
-        }
-    }
+    //        // Step 4: Invoke event safely
+    //        FileUploaded?.Invoke(this, new FileEventArgs(fileName));
+    //    }
+    //}
 
-    // Subscriber
-    public class Logger
-    {
-        public void OnFileUploaded(object sender, FileEventArgs e)
-        {
-            Console.WriteLine($"[Logger] File uploaded: {e.FileName}");
-        }
-    }
+    //// Subscriber
+    //public class Logger
+    //{
+    //    public void OnFileUploaded(object sender, FileEventArgs e)
+    //    {
+    //        Console.WriteLine($"[Logger] File uploaded: {e.FileName}");
+    //    }
+    //}
 
     public class Program
     {
         public static void Main()
         {
-            FileUploader uploader = new FileUploader();
-            Logger logger = new Logger();
+            //FileUploader uploader = new FileUploader();
+            //Logger logger = new Logger();
 
-            // Subscribe
-            uploader.FileUploaded += logger.OnFileUploaded;
+            //// Subscribe
+            //uploader.FileUploaded += logger.OnFileUploaded;
 
-            // Trigger event
-            uploader.Upload("report.pdf");
+            //// Trigger event
+            //uploader.Upload("report.pdf");
+            int a = 10;
+            int b = 20;
+            int c = a + b;
+            Console.WriteLine(c);
             Console.ReadLine();
         }
     }
---
+++ ev-3-a/EventsAndDelegates/EventsAndDelegates/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventsAndDelegates
+{
+    public class Publisher1
+    {
+        public delegate void NotifyHandler(string message);
+        public event NotifyHandler OnNotify;
+        //public event Action<string> OnNotify;
+        public void SendMessage(string msg)
+        {
+            OnNotify?.Invoke(msg);
+        }
+    }
+
+    public class Publisher2
+    {
+        public event EventHandler OnNotify;
+        public void SendMessage()
+        {
+            OnNotify?.Invoke(this, EventArgs.Empty);
+        }
+    }
+
+    public class MessageClass:EventArgs
+    {
+        public string Message { get; set; }
+    }
+
+    public class Publishere3
+    {
+        public event EventHandler<MessageClass> OnNotify;
+        public void SendMessage(MessageClass data)
+        {
+            OnNotify?.Invoke(this, data);
+        }
+    }
+
+    public class Program
+    {
+        static void Main()
+        {
+            Publisher1 publisher1 = new Publisher1();
+            publisher1.OnNotify += ShowMessage1;
+            publisher1.SendMessage("hello1");
+
+            Publisher2 publisher2 = new Publisher2();
+            publisher2.OnNotify +=ShowMessage2 ;
+            publisher2.SendMessage();
+
+            Publishere3 publishere3 = new Publishere3();
+            publishere3.OnNotify += ShowMessage3;
+            publishere3.SendMessage(new MessageClass { Message = "hello3" });
+            Console.ReadLine();
+        }
+
+        private static void ShowMessage3(object sender, MessageClass e)
+        {
+            Console.WriteLine(e.Message);
+            Console.WriteLine(sender.GetType().Name);
+        }
+
+        private static void ShowMessage2(object sender, EventArgs e)
+        {
+            Console.WriteLine("hello2");
+        }
+
+        static void ShowMessage1(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+
+}
